a20163b686cd3a1639774b4411ccc4d872ec63ba
nfooteaz <nfoote.alpha@gmail.com>
2018-02-10 15:52:37 -0500

    Blank Project

---
+++ ShelterMap/app/src/main/java/team31/cs2340/gatech/sheltermap/LoginActivity.java
@@ -0,0 +1,13 @@
+package team31.cs2340.gatech.sheltermap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
