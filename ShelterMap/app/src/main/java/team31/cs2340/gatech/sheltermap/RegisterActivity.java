f0972b3f3b6e90039dc558bee0cd63bbcfaef375
Nathan <nfooteaz@gmail.com>
2018-02-20 21:06:50 -0500

    implement Register

--- ShelterMap/app/src/main/java/team31/cs2340/gatech/sheltermap/RegisterActivity.java
+++ ShelterMap/app/src/main/java/team31/cs2340/gatech/sheltermap/RegisterActivity.java
@@ -1,6 +1,5 @@
 package team31.cs2340.gatech.sheltermap;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -10,23 +9,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    private EditText name;
+    private EditText phone;
     private Button cancel;
     private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
-
-        final TextView errorText = (TextView) findViewById(R.id.error);
-        errorText.setTextColor(Color.RED);
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
 
         cancel = (Button) findViewById(R.id.cancel);
         submit = (Button) findViewById(R.id.submit);
@@ -39,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
             // https://androidsolved.wordpress.com/2015/07/01/how-to-move-from-one-activityscreen-to-another-activityscreen-in-android/
             @Override
             public void onClick(View view) {
-                // TODO Auto-generated method stub
+
                 Intent go2Welcome = new Intent(getApplicationContext(), WelcomeActivity.class);
                 startActivity(go2Welcome);
             }
@@ -47,21 +47,19 @@ public class LoginActivity extends AppCompatActivity {
 
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                boolean fail = true;
+                boolean fail = false;
                 for(User u: User.users){
                     if(u.getEmail().equals(email.getText().toString())){
-                        if(u.getPassword().equals(password.getText().toString())){
-                            //login success
-                            Intent go2Account = new Intent(LoginActivity.this, AccountActivity.class);
-                            startActivity(go2Account);
-                            fail = false;
-                        }
+                        fail = true;
                     }
                 }
-                if(fail) {
-                    //login fail
-                    errorText.setVisibility(View.VISIBLE);
+                if(!fail){
+                    User newUser = new User(name.getText().toString(),email.getText().toString(),password.getText().toString(),phone.getText().toString());
+                    User.users.add(newUser);
+                    Intent go2Account = new Intent(RegisterActivity.this, AccountActivity.class);
+                    startActivity(go2Account);
                 }
+                //Register fail - duplicate email
             }
         });
 
