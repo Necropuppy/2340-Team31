40f8e424e91f0b786eae37b9b79b85c604198000
egonzalez49 <egonzalez49@gatech.edu>
2018-04-23 10:10:55 -0400

    Kinda fixed up the detailed view

--- ShelterMap/app/src/main/java/team31/cs2340/gatech/sheltermap/DetailedViewActivity.java
+++ ShelterMap/app/src/main/java/team31/cs2340/gatech/sheltermap/DetailedViewActivity.java
@@ -37,26 +37,26 @@ public class DetailedViewActivity extends AppCompatActivity{
         Shelter shelter = Shelter.shelters.get(position);
         Log.d(AccountActivity.TAG, "Successful extra");
         TextView name = (TextView) findViewById(R.id.shelter_name);
-        String name1 = "Shelter Name: " + shelter.getName();
+        String name1 = shelter.getName();
         name.setText(name1);
         Log.d(AccountActivity.TAG, "Successful name");
         TextView key = (TextView) findViewById(R.id.shelter_key);
-        String name2 = "Shelter ID: " + shelter.getKey();
+        String name2 = "" + shelter.getKey();
         key.setText(name2);
         TextView capacity = (TextView) findViewById(R.id.shelter_cap);
-        String name3 = "Capacity: " + shelter.getPop() + "/" + shelter.getCap();
+        String name3 = "" + shelter.getPop() + "/" + shelter.getCap();
         capacity.setText(name3);
         TextView restriction = (TextView) findViewById(R.id.shelter_restriction);
-        String name4 = "Restrictions: " + shelter.getRestriction();
+        String name4 = "" + shelter.getRestriction();
         restriction.setText(name4);
         TextView longitude = (TextView) findViewById(R.id.shelter_longitude);
-        String name5 = "Longitude: " + shelter.getLongitude();
+        String name5 = "" + shelter.getLongitude();
         longitude.setText(name5);
         TextView latitude = (TextView) findViewById(R.id.shelter_latitude);
-        String name6 = "Latitude: " + shelter.getLatitude();
+        String name6 = "" + shelter.getLatitude();
         latitude.setText(name6);
         TextView address = (TextView) findViewById(R.id.shelter_address);
-        String name7 = "Shelter Name: " + shelter.getAddress();
+        String name7 = "" + shelter.getAddress();
         address.setText(name7);
 
 
